1e0f14ed337e9eca38446abe9bfb9bc03ba4178e
Dmitry <114141749+dmitrybd2004@users.noreply.github.com>
2025-05-13 17:12:53 +0000

    part 2, distance calc

--- 2039.cpp
+++ 2039.cpp
@@ -9,18 +9,38 @@ public:
             adj[edges[i][1]].push_back(edges[i][0]);
         }
 
-        for (int i = 0; i < adj.size(); ++i) {
-            cout << "Node " << i << ": ";
-            for (int j = 0; j<adj[i].size();++j) {
-                cout << adj[i][j] << " ";
-            }
-            cout << "\n";
-        }
+        // for (int i = 0; i < adj.size(); ++i) {
+        //     cout << "Node " << i << ": ";
+        //     for (int j = 0; j<adj[i].size();++j) {
+        //         cout << adj[i][j] << " ";
+        //     }
+        //     cout << "\n";
+        // }
 
         vector<bool> visited(sz);
         vector<int> distance (sz);
         queue<int> q;
+        
+        for(int i = 0;i<sz;++i){
+            distance[i] = -1;
+        }
+        q.push(0);
+        distance[0] = 0;
+        while(!q.empty()){
+            int cur_node = q.front();
+            int cur_distance = distance[cur_node];
+            for(int i = 0;i<adj[cur_node].size();++i){
+                if(distance[adj[cur_node][i]] == -1){
+                    distance[adj[cur_node][i]] = cur_distance + 1;
+                    q.push(adj[cur_node][i]);
+                }
+            }
+            q.pop();
+        }
 
+        for(int i = 0;i<sz;++i){
+            std::cout<<"dist "<< i << ":"<<" "<<distance[i]<<"\n";
+        }
         return 0;
     }
 };
